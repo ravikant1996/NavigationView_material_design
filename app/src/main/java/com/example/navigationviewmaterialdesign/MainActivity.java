9494537e3e1794b743235d7652b1006d8be579af
Kant.ravi747@gmail.com <46792393+ravikant1996@users.noreply.github.com>
2020-11-08 23:00:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigationviewmaterialdesign/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.navigationviewmaterialdesign;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout= findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView= findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+
+        NavController navController= Navigation.findNavController(this, R.id.navigationFragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
+
+        final TextView textView= findViewById(R.id.txtTitle);
+
+        navController.addOnDestinationChangedListener(new NavController.OnDestinationChangedListener() {
+            @Override
+            public void onDestinationChanged(@NonNull NavController controller, @NonNull NavDestination destination, @Nullable Bundle arguments) {
+                textView.setText(destination.getLabel());
+            }
+        });
+    }
+}
